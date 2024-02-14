5153bfeef6409edb4df9cbf04013c7acfc19812d
Keng <kengthao57@gmail.com>
2024-02-14 17:35:38 -0600

    added typeDefs as well as auth

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,38 @@
+const typeDefs = `
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    bookCount: int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user(userId: ID!): User
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(bookId: ID!, authors: [String], description: String, title: String, image: String, link: String): Book
+    removeBook(bookId: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
